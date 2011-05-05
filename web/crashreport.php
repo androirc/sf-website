908ecec155d008643f20ff3ec1973f8489210e83
MewT <mewt@androirc.com>
2011-05-05 22:41:25 +0200

    updated web folder for the migration

--- web/crashreport.php
+++ web/crashreport.php
@@ -1,71 +1,36 @@
 <?php
 
-  if ((! isset($_POST['phone_model'])) || (! isset($_POST['android_version'])) ||
-      (! isset($_POST['thread_name'])) || (! isset($_POST['error_message'])) || (! isset($_POST['callstack'])))
-      die('Bad parameters');
-  
-  require('includes/sqlmanager.class.php');
-  $db = new SQLManager();
-  
-  if (isset($_POST['version'])) $version = $db->protect($_POST['version']);
-  else $version = 'Unknown';
+/*
+ * This file is part of the AndroIRC website.
+ *
+ * (c) 2010-2011 Julien Brochet <mewt@androirc.com>
+ * (c) 2010-2011 Sébastien Brochet <blinkseb@androirc.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
-  $pm =         $db->protect($_POST['phone_model']);
-  $av =         $db->protect($_POST['android_version']);
-  $tn =         $db->protect($_POST['thread_name']);
-  $em =         $db->protect($_POST['error_message']);
-  $callstack =  $db->protect($_POST['callstack']);
-  $cs = $_POST['callstack'];
-  $ip = $_SERVER["REMOTE_ADDR"];
-  $host = gethostbyaddr($ip);
-  
-  $query = $db->query("SELECT COUNT(*) AS nbr FROM andro_crashreport WHERE crCallstack='$callstack'");
-  $data = mysql_fetch_object($query);
-  
-  $bool = 0;
-  
-  if ($data->nbr == 0)
-  {
-    $bool = 1;
-  }
-  
-      $query = $db->query("INSERT INTO andro_crashreport (crPhoneModel, crAndroidVersion, crThreadName, crErrorMessage, crCallstack, crTime, crAndroIRCVersion) VALUES('$pm', '$av', '$tn', '$em', '$callstack', NOW(), '$version')");
+$c = curl_init();
 
-      if ( !preg_match('#sdk#', $pm))
-      {
-          /* Ce n'est pas l'émulateur ... */
-          require_once 'includes/libmail.class.php';
+curl_setopt($c, CURLOPT_URL, 'http://www.androirc.com/crashreport');
+curl_setopt($c, CURLOPT_POST, true);
+curl_setopt($c, CURLOPT_POSTFIELDS, array(
+    'phone_model' => get('phone_model'),
+    'android_version' => get('android_version'),
+    'thread_name' => get('thread_name'),
+    'error_message' => get('error_message'),
+    'callstack' => get('callstack'),
+    'version' => get('version')
+ ));
 
-          $mail = new Mail();
-          $id = mysql_insert_id();
+curl_exec ($c);
+curl_close ($c); 
 
-          $mail->From('noreply@androirc.com');
-          $mail->To('contact@androirc.com');
-
-          $mail->Subject("[Crash] Rapport de crash #$id ($version)");
-
-    $message = "Nouveau rapport de crash :
-
-            Phone model : $pm
-            Android version : $av
-            AndroIRC version : $version
-            Thread name : $tn
-            Error message : $em
-            IP Address: $ip
-            Host: $host
-            Callstack : 
-          
-            $cs
-
-    Vous pouvez supprimer ce rapport directement via le lien suivant : http://www.androirc.com/admin/crashes.php?rm=$id
-    --
-    AndroIRC";
-
-          $mail->Body($message);
-
-          $mail->Send();
-      }
-
-      if (mysql_affected_rows() == 1) echo 'ok';
-    //}
-?>
+function get($name, $default = '')
+{
+    if (isset($_POST[$name])) {
+        return $_POST[$name];
+    }
+    
+    return $default;
+}
--- web/tips.php
+++ web/tips.php
@@ -1,9 +1,19 @@
-﻿<?php
+<?php
 
-$lang = $_GET['lang'];
-$date = $_GET['date'];
+/*
+ * This file is part of the AndroIRC website.
+ *
+ * (c) 2010-2011 Julien Brochet <mewt@androirc.com>
+ * (c) 2010-2011 Sébastien Brochet <blinkseb@androirc.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
-$url = '/tip/' . $lang;
+$lang = get('lang', 'en');
+$date = get('date');
+
+$url = 'http://www.androirc.com/tip/' . $lang;
 
 if ($date) {
     $url .= '/' . $date;
@@ -12,3 +22,11 @@ if ($date) {
 header('Status: 301 Moved Permanently', false, 301);   
 header('Location: ' . $url);   
 
+function get($name, $default = '')
+{
+    if (isset($_GET[$name])) {
+        return $_GET[$name];
+    }
+    
+    return $default;
+}
