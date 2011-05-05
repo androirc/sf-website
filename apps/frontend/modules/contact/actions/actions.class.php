a2db8e4a4a98693c34bd084919c60cd4fc3f8d42
MewT <mewt.fr@gmail.com>
2011-05-05 17:24:26 +0200

    changed the contact form e-mail

--- apps/frontend/modules/contact/actions/actions.class.php
+++ apps/frontend/modules/contact/actions/actions.class.php
@@ -26,21 +26,27 @@ class contactActions extends sfActions
                 $user_host = gethostbyaddr($user_ip);
                 $datas = $request->getParameter('contact');
                 
-                // Envoie de l'email à contact@androirc.com
+                $text = <<<EOF
+Bonjour,
+{$datas['name']} vient d'utiliser le formulaire du site web.
+                
+    Son adresse IP :   {$user_ip}
+    Son host :         {$user_host}
+    
+Voici son message :
+    
+    {$datas['message']}
+    
+--
+AndroIRC
+    
+EOF;
                 
                 $message = $this->getMailer()->compose(
                     array('noreply@androirc.com' => 'AndroIRC'),
                     'contact@androirc.com',
                     '[Contact] Nouveau message du site web',
-                    <<<EOF
-{$datas['name']} vient d'utiliser le formulaire du site web. Voici son message :
- 
-{$datas['message']}
- 
-Ses informations : Adresse IP : {$user_ip} - Host : {$user_host}
---
-AndroIRC
-EOF
+                    $text
                 );
                 
                 $message->setReplyTo($datas['email']);
