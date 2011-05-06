c0b44684f6ac0155041a9a7d58d9427edd6cbd8b
MewT <mewt.fr@gmail.com>
2011-05-06 12:00:36 +0200

    fixed typo

--- apps/frontend/modules/beta/templates/indexSuccess.php
+++ apps/frontend/modules/beta/templates/indexSuccess.php
@@ -2,7 +2,7 @@
 
 <h2>AndroIRC Beta version</h2>
 
-<p>You can now download the last beta of AndroIRC! This file is not available in the Android Market because <strong>it still contains bugs</strong>. Keep in mind that it's just a beta release ;). If you find a bug, you can contact us at <a href="mailto:contact@androirc.com">contact@androirc.com</a> or just post a new ticket in <a href="http://bugs.androirc.com/">the bug tracker</a>. Thanks!</p>
+<p>You can now download the lastest beta of AndroIRC! This file is not available in the Android Market because <strong>it still contains bugs</strong>. Keep in mind that it's just a beta release ;). If you find a bug, you can contact us at <a href="mailto:contact@androirc.com">contact@androirc.com</a> or just post a new ticket in <a href="http://bugs.androirc.com/">the bug tracker</a>. Thanks!</p>
 
 <div id="beta">
     <?php if (false === $beta): ?>
