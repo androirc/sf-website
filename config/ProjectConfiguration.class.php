929f1f12bd7416abd35272ecaccd054d71a8db38
MewT <mewt.fr@gmail.com>
2011-05-14 18:01:09 +0200

    removed useless code

--- config/ProjectConfiguration.class.php
+++ config/ProjectConfiguration.class.php
@@ -11,36 +11,5 @@ class ProjectConfiguration extends sfProjectConfiguration
         $this->enablePlugins('sfDoctrinePlugin');
         $this->enablePlugins('sfDoctrineGuardPlugin');
         $this->enablePlugins('sfAnotherReCaptchaPlugin');
-
-        $this->dispatcher->connect('request.filter_parameters', array($this, 'filterRequestParameters'));
-    }
-    
-    public function filterRequestParameters(sfEvent $event, $parameters)
-    {
-        $request = $event->getSubject();
-        
-         $list = array('iPhone','Android','MIDP','Opera Mobi',  
-                      'Opera Mini','BlackBerry','HP iPAQ','IEMobile',  
-                      'MSIEMobile','Windows Phone','HTC','LG',  
-                      'MOT','Nokia','Symbian','Fennec',  
-                      'Maemo','Tear','Midori','armv',  
-                      'Windows CE','WindowsCE','Smartphone','240x320',  
-                      '176x220','320x320','160x160','webOS',  
-                      'Palm','Sagem','Samsung','SGH',  
-                      'SIE','SonyEricsson','MMP','UCWEB');
-         
-         $ua = $request->getHttpHeader('User-Agent');
-         
-         $onMobile = false;
-         
-         foreach($list as $tmp)
-         {
-             if (preg_match('#'. $tmp .'#', $ua)) {
-                 $onMobile = true;
-                 break;
-             }
-         }
-
-         return $parameters;
     }
 }
