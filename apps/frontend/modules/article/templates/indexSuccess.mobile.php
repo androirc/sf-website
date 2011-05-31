193d2f6aa764f8b88ef75c4efea1e20d01f4ae0b
MewT <mewt.fr@gmail.com>
2011-05-31 17:48:52 +0200

    added disqus support for mobile version

--- apps/frontend/modules/article/templates/indexSuccess.mobile.php
+++ apps/frontend/modules/article/templates/indexSuccess.mobile.php
@@ -4,3 +4,16 @@
 <?php foreach ($articles as $article): ?>
     <?php include_partial('article/article', array('article' => $article, 'sumup' => true)) ?>
 <?php endforeach; ?> 
+
+<script type="text/javascript">
+    /* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
+    var disqus_shortname = 'androirc'; // required: replace example with your forum shortname
+
+    /* * * DON'T EDIT BELOW THIS LINE * * */
+    (function () {
+        var s = document.createElement('script'); s.async = true;
+        s.type = 'text/javascript';
+        s.src = 'http://' + disqus_shortname + '.disqus.com/count.js';
+        (document.getElementsByTagName('HEAD')[0] || document.getElementsByTagName('BODY')[0]).appendChild(s);
+    }());
+</script>
