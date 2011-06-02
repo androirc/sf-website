ddb6451d20376123451e0f58b8962556479364f2
MewT <mewt.fr@gmail.com>
2011-06-02 14:52:21 +0200

    fixed typo

--- apps/frontend/modules/beta/templates/indexSuccess.php
+++ apps/frontend/modules/beta/templates/indexSuccess.php
@@ -2,13 +2,13 @@
 
 <h2>AndroIRC Beta version</h2>
 
-<p>You can now download the lastest beta of AndroIRC! This file is not available in the Android Market because <strong>it still contains bugs</strong>. Keep in mind that it's just a beta release ;). If you find a bug, you can contact us at <a href="mailto:contact@androirc.com">contact@androirc.com</a> or just post a new ticket in <a href="http://bugs.androirc.com/">the bug tracker</a>. Thanks!</p>
+<p>You can now download the latest beta of AndroIRC! This file is not available in the Android Market because <strong>it still contains bugs</strong>. Keep in mind that it's just a beta release ;). If you find a bug, you can contact us at <a href="mailto:contact@androirc.com">contact@androirc.com</a> or just post a new ticket in <a href="http://bugs.androirc.com/">the bug tracker</a>. Thanks!</p>
 
 <div id="beta">
     <?php if (false === $beta): ?>
         No beta right now... come back later!
     <?php else: ?>
-        <a class="button blue" href="<?php echo url_for('beta_download') ?>">Download the last beta of AndroIRC</a><br />
+        <a class="button blue" href="<?php echo url_for('beta_download') ?>">Download the latest beta of AndroIRC</a><br />
         <span>(Version <?php echo $beta->getVersion() ?> - <?php echo $beta->getDateTimeObject('created_at')->format('m/d/Y') ?>)</span>
     <?php endif ?>
 </div>
