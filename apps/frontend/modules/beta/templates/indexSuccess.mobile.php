0a6c98de7bf3b260cc24719c6edf488fa6a2f7cd
MewT <mewt.fr@gmail.com>
2011-06-02 18:33:45 +0200

    fixed beta page

--- apps/frontend/modules/beta/templates/indexSuccess.mobile.php
+++ apps/frontend/modules/beta/templates/indexSuccess.mobile.php
@@ -4,7 +4,7 @@
 
 <p>You can now download the latest beta of AndroIRC! This file is not available in the Android Market because <strong>it still contains bugs</strong>. Keep in mind that it's just a beta release ;). If you find a bug, you can contact us at <a href="mailto:contact@androirc.com">contact@androirc.com</a> or just post a new ticket in <a href="http://bugs.androirc.com/">the bug tracker</a>. Thanks!</p>
 
-<div class="center">
+<div id="beta">
     <?php if (false === $beta): ?>
         No beta right now... come back later!
     <?php else: ?>
