7b768b5b8c3cd1cbd126b43464f5a1d0a37f72ff
MewT <mewt.fr@gmail.com>
2011-06-05 21:27:07 +0200

    fixed typo

--- apps/frontend/lib/MobileFilter.class.php
+++ apps/frontend/lib/MobileFilter.class.php
@@ -22,7 +22,16 @@ class MobileFilter extends sfFilter
 
             $user->setFrom(null);
 
-            $uamatches = array('midp', 'j2me', 'avantg', 'docomo', 'novarra', 'palmos', 'palmsource', '240x320', 'opwv', 'chtml', 'pda', 'windows ce', 'mmp\/', 'blackberry', 'mib\/', 'symbian', 'wireless', 'nokia', 'hand', 'mobi', 'phone', 'cdm', 'up\.b', 'audio', 'SIE\-', 'SEC\-', 'samsung', 'HTC', 'mot\-', 'mitsu', 'sagem', 'sony', 'alcatel', 'lg', 'erics', 'vx', 'NEC', 'philips', 'mmm', 'xx', 'panasonic', 'sharp', 'wap', 'sch', 'rover', 'pocket', 'benq', 'java', 'pt', 'pg', 'vox', 'amoi', 'bird', 'compal', 'kg', 'voda', 'sany', 'kdd', 'dbt', 'sendo', 'sgh', 'webos');
+            $uamatches = array(
+                'midp', 'j2me', 'avantg', 'docomo', 'novarra', 'palmos',
+                'palmsource', '240x320', 'opwv', 'chtml', 'pda', 'windows ce',
+                'mmp\/', 'blackberry', 'mib\/', 'symbian', 'wireless', 'nokia',
+                'hand', 'mobi', 'phone', 'cdm', 'up\.b', 'audio', 'SIE\-', 'SEC\-', 
+                'samsung', 'HTC', 'mot\-', 'mitsu', 'sagem', 'sony', 'alcatel', 'lg', 
+                'erics', 'vx', 'NEC', 'philips', 'mmm', 'xx', 'panasonic', 'sharp',
+                'wap', 'sch', 'rover', 'pocket', 'benq', 'java', 'pt', 'pg', 'vox', 
+                'amoi', 'bird', 'compal', 'kg', 'voda', 'sany', 'kdd', 'dbt', 'sendo', 'sgh', 'webos'
+            );
 
             foreach ($uamatches as $uastring) {
                 if (preg_match('/' . $uastring . '/i', $request->getHttpHeader('User-Agent'))) {
