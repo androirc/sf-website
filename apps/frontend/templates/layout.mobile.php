53d73fc59dd7193f50cd78384a872dd82c6e27c3
MewT <julien@brochet.me>
2011-06-08 01:05:55 +0200

    added own google analytics account for the mobile version

--- apps/frontend/templates/layout.mobile.php
+++ apps/frontend/templates/layout.mobile.php
@@ -46,14 +46,15 @@
             Copyright &copy; AndroIRC.com. All rights reserved. Powered by <?php echo image_tag('symfony.gif', array('class' => 'famfamfam')) ?>
         </div>
         <script type="text/javascript">
-            var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
-            document.write(unescape("%3Cscript src=\'" + gaJsHost + "google-analytics.com/ga.js\' type=\'text/javascript\'%3E%3C/script%3E"));
-        </script>
-        <script type="text/javascript">
-            try {
-                var pageTracker = _gat._getTracker("UA-133630-2");
-                pageTracker._trackPageview();
-            } catch(err) {}
+            var _gaq = _gaq || [];
+            _gaq.push(['_setAccount', 'UA-133630-5']);
+            _gaq.push(['_trackPageview']);
+
+            (function() {
+            var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+            ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+            var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+            })();
         </script>
     </body>
 </html>
