40cdb8aee51bd3a106db1e63e4f7ea58bc28d2a6
Julien Brochet <mewt@madalynn.eu>
2011-10-30 14:34:15 +0100

    refactored screenshot section

--- apps/frontend/modules/screenshots/templates/indexSuccess.php
+++ apps/frontend/modules/screenshots/templates/indexSuccess.php
@@ -1,9 +1,38 @@
 <?php slot('title','Screenshots') ?>
-<?php use_javascript('simpleviewer.js') ?>
+<?php use_javascript('jquery.bxSlider.min.js', 'first') ?>
 
 <script type="text/javascript">
-	jQuery(document).ready(function () {
-		SV.simpleviewer.load('sv-container', '100%', '680', 'ffffff', true);
-	});
+    $(document).ready(function() {
+        var slider = $('#slider').bxSlider({
+            controls: false
+        });
+
+        $('.thumbs a').click(function(){
+            var thumbIndex = $('.thumbs a').index(this);
+            slider.goToSlide(thumbIndex);
+
+            $('.thumbs a').removeClass('pager-active');
+            $(this).addClass('pager-active');
+
+            return false;
+        });
+
+        $('.thumbs a:first').addClass('pager-active');
+    });
 </script>
-<div id="sv-container"></div>
+
+<ul id="slider">
+    <?php for ($i = 1 ; $i < 29 ; $i++): ?>
+        <li>
+            <div class="device">
+                <?php echo image_tag('device/device-' . sprintf("%02d", $i) .'.png') ?>
+            </div>
+        </li>
+    <?php endfor; ?>
+</ul>
+
+<div class="thumbs">
+    <?php for ($i = 1 ; $i < 29 ; $i++): ?>
+        <a href="#"><?php echo image_tag('device/thumbnail/device-' . sprintf("%02d", $i) .'.png') ?></a>
+    <?php endfor; ?>
+</div>
